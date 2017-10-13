4f4880cb929886d109e12dd96d7a1b6c7b9d7f21
Manuel <manuelgomezsuarez@gmail.com>
2017-10-13 11:27:12 +0200

    e

--- AII-Ejercicios/Practica1/pracica1.py
+++ AII-Ejercicios/Practica1/pracica1.py
@@ -6,18 +6,45 @@ Created on 13 oct. 2017
 '''
 import Tkinter as tk
 import sqlite3
+import feedparser
+from pattern.web import Link
 
+
+url="http://www.us.es/rss/feed/portada"
+rss=feedparser.parse(url)
+entradas=rss.entries
 
 def almacenaNoticias():
     conn=sqlite3.connect("noticias")
-    conn.execute('''CREATE TABLE NOTICIAS
-         (ID INT PRIMARY KEY     NOT NULL,
-         TITULO           TEXT    NOT NULL,
-         LINK            TEXT     NOT NULL,
-         FECHA        CHAR(50));''')
-    print "Table created successfully";
-     
-    conn.commit()
+    
+    try:
+        conn.execute('''CREATE TABLE NOTICIAS
+             (ID INT PRIMARY KEY     NOT NULL,
+             TITULO           TEXT    NOT NULL,
+             LINK            TEXT     NOT NULL,
+             FECHA        TEXT    NOT NULL);''')
+        conn.commit()
+        print "Table created successfully";
+    except:
+        print("Ya estaba creada la tabla")
+        pass
+    idNoticia=0
+    for noticia in entradas:
+        titulo=str(noticia.title.encode('utf-8'))
+        link=str(noticia.link.encode('utf-8'))
+        fecha=str(noticia.published_parsed[2])+"/" +str(noticia.published_parsed[1])+"/" +str(noticia.published_parsed[0])
+        conn.execute("INSERT INTO NOTICIAS (ID,TITULO,LINK,FECHA) \
+           VALUES ("+str(idNoticia)+",'"+titulo+"','"+link+"' ,'"+fecha+"')");
+        conn.commit  
+        
+
+#         print(titulo)
+#         print(link)
+#         print(fecha)
+#         print(idNoticia)
+        idNoticia+=1
+    print("Noticias guardadas correctamente")
+
 
 ventana = tk.Tk()
 
